e9dc8b3dadd2ce0b4fa0efd7869bf1ff6cea62a2
mwq2020 <mwq2020@163.com>
2018-10-31 16:33:41 +0800

    fix

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -3,17 +3,40 @@
 use Yaf\Controller_Abstract;
 class IndexController extends Controller_Abstract 
 {
-	public function indexAction(){
+    public function init() {
+           $this->initView();
+        }
 
-		echo "this is a test page";
-		exit;
+
+	public function indexAction()
+    {
+
+        echo  $this->getView()->getScriptPath();
+        echo "<hr>";
+
+
+        $params = array(
+               'name' => 'value',
+           );
+
+        // 模板变量赋值
+        $this->getView()->assign($params)->assign("foo", "bar");
+
+        // 获取模板变量的值
+        echo $this->_view->get("name");
+        echo "<hr>";
+
+        echo $this->getView()->get("name");
+        echo "<hr>";
+
+        // 输出模板页面
+        return $this->getView()->render("index/index.phtml");
 	}
 
 	public function pageAction()
 	{
 		echo "this is a test view  page";
-		exit
-
+		exit;
 	}
 
 }
